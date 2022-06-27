3f8c24ce547cfe473f8112a92b86df92df360bef
ryannmx <ryanshift22@gmail.com>
2022-06-27 17:06:24 -0300

    Desenvolvendo uma aplicação completa com ReactJS - Ignite-lab

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ApolloProvider } from "@apollo/client"
+import { BrowserRouter } from "react-router-dom"
+import { Router } from "./components/Router"
+import { client } from "./lib/apollo"
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
+  )
+}
+
+export default App
---
+++ src/pages/Subscribe.tsx
@@ -0,0 +1,81 @@
+import { useNavigate } from "react-router-dom";
+import { FormEvent, useState } from "react";
+import { Logo } from "../components/Logo";
+import { useCreateSubscriberMutation } from "../graphql/generated";
+
+export function Subscribe() {
+  const navigate = useNavigate()
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const [CreateSubscriber, { loading }] = useCreateSubscriberMutation()
+
+  async function handleSubscribe(event: FormEvent) {
+    event.preventDefault();
+
+    await CreateSubscriber({
+      variables: {
+        name,
+        email,
+      }
+    })
+
+    navigate('/event')
+  }
+
+  return (
+
+    <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
+
+      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
+        <div className="max-w-[640px]">
+          <Logo />
+
+          <h1 className="mt-8 text-[2.5rem] leading-tight"><strong className="text-blue-500">Construa uma aplicação</strong>  completa, do zero, com <strong className="text-blue-500">React</strong></h1>
+
+          <p className="mt-4 text-gray-200 leading-relaxed">
+            Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
+          </p>
+
+        </div>
+        <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+
+          <strong className="text-2xl mb-6 block">Increva-se gratuitamente</strong>
+
+          <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full ">
+            <input
+
+              className="bg-gray-900 rounded px-5 h-14"
+              type="text"
+              placeholder="Seu nome completo" name="" id=""
+              onChange={event => setName(event.target.value)}
+            />
+            <input
+
+              className="bg-gray-900 rounded px-5 h-14"
+              type="email"
+              placeholder="Digite seu e-mail" name="" id=""
+              onChange={event => setEmail(event.target.value)}
+
+            />
+
+            <button
+              disabled={loading}
+              type="submit"
+              className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
+            >Garantir minha vaga!
+
+            </button>
+
+          </form>
+
+        </div>
+      </div>
+
+      <img src="src/assets/code-mokup.png" className="mt-10" alt="" />
+
+    </div>
+
+  );
+}
